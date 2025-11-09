e7453192a9ac5530075b9b55b2085e6d647426b2
Sarth Satpute <152148836+SarthSatpute@users.noreply.github.com>
2025-11-09 22:41:11 +0530

    Add files via upload

---
+++ py3dbc/__init__.py
@@ -0,0 +1,15 @@
+
+__version__ = '0.1.0'
+__author__ = 'Sarth!'
+
+from py3dbp.main import Packer, Bin, Item
+from py3dbp.main import Painter
+from py3dbc.maritime.container import MaritimeContainer
+
+__all__ = [
+    'Packer',
+    'Bin', 
+    'Item',
+    'Painter',
+    'MaritimeContainer'
+]
