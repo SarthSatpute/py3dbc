a693b26500bfaa1d3997d6f29a2e9480d96562ef
Sarth Satpute <152148836+SarthSatpute@users.noreply.github.com>
2025-11-13 23:00:22 +0530

    bigmovves

--- py3dbc/__init__.py
+++ py3dbc/__init__.py
@@ -1,15 +1,24 @@
+"""
+py3dbc - 3D Bin Packing for Containers
+Maritime container ship load optimization with stability physics
+"""
 
-__version__ = '0.1.0'
-__author__ = 'Sarth!'
+__version__ = "1.0.0"
+__author__ = "Sarth Satpute"
+__license__ = "MIT"
 
-from py3dbp.main import Packer, Bin, Item
-from py3dbp.main import Painter
+# Import main classes for easy access
 from py3dbc.maritime.container import MaritimeContainer
+from py3dbc.maritime.ship import ContainerShip, Slot
+from py3dbc.maritime.packer import MaritimePacker
+from py3dbc.maritime.constraints import MaritimeConstraintChecker
+from py3dbc.physics.stability import StabilityCalculator
 
 __all__ = [
-    'Packer',
-    'Bin', 
-    'Item',
-    'Painter',
-    'MaritimeContainer'
+    "MaritimeContainer",
+    "ContainerShip",
+    "Slot",
+    "MaritimePacker",
+    "MaritimeConstraintChecker",
+    "StabilityCalculator",
 ]
